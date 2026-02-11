2fd671c1ef0bc453170dd8d39c127b75302abe53
Sh <mishrashaswat29@gmail.com>
2026-02-11 23:43:06 +0530

    hooks

--- Xebia/testreact/xfeb9/src/components/UserCard.jsx
+++ Xebia/testreact/xfeb9/src/components/UserCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { SquareX } from 'lucide-react'
 
-function UserCard({data}) {
+function UserCard({ id, data, deleteUser }) {
+
   return (
     <div className='bg-green-400 p-4 m-4 rounded-lg  m-5 shadow-green-700 shadow-md'>
-        <h3 className='text-lg font-bold text-gray-800'>NAME:{data.name}</h3> 
+      <div>
+        <button className='bg-red-500 text-white px-4 py-2 rounded-full' onClick={() => deleteUser(id)}><SquareX /></button>
+      </div>
+       <div><h3 className='text-lg font-bold text-gray-800'>NAME:{data.name}</h3> 
         <p className='text-md font-medium text-gray-800'>AGE:{data.age}</p>
         <p className='text-md font-medium text-gray-800'>CITY:{data.city}</p>
         <p className='text-md font-medium text-gray-800'>TEAM:{data.team}</p>
+        </div>
+        
     </div>
   )
 }
